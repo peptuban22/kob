35c4cda793a884508d29826cdc8f316f978e55c6
peptuban22 <pep.tuban@gmail.com>
2022-05-17 11:40:48 +0700

    menambahkan kategori ovh area

--- tahun_2021.R
+++ tahun_2021.R
@@ -41,9 +41,67 @@ order_area <- order_and_cc %>%
               mutate(Jenis.aktivitas = case_when(str_detect(Description,".[o|O][v|V][h|H].|[o|O][v|V][h|H]") ~ "Overhoule",
                                                  str_detect(Description,".[o|O][v|V][e|E][r|R][h|H].|[o|O][v|V][e|E][r|R][h|H]") ~ "Overhoule",
                                                  str_detect(Description,".[p|P][a|A][t|T][c|C].|[p|P][a|A][t|T][c|C]") ~ "Patchjob",
-                                                 TRUE ~ "Operasional")) %>%
-  write.csv("daftar_order.csv")
+                                                 TRUE ~ "Operasional")) 
+ 
 
+
+
+
+#Daftar lokasi ovh kiln 
+area_kiln1 <- c("COAL MILL T1SIE RKC1",
+                "CRUSHER BT. KAPUR T1",
+                "CRUSHER TNH LIAT T1",
+                "KILN T1 - SIE RKC1",
+                "RAW MILL T1 SIE RKC1"
+)
+
+#Daftar lokasi ovh kiln 2
+area_kiln2 <- c("COAL MILL T2 SI RKC2",
+                "CRUSHER BT. KAPUR T2",
+                "CRUSHER TNH LIAT T2",
+                "KILN T2 - SIE RKC2",
+                "RAW MILL T2 SIE RKC2"
+)
+
+#Daftar lokasi ovh kiln 3
+area_kiln3 <- c("COAL MILL T3 SI RKC3",
+                "CRUSHER BT. KAPUR T3",
+                "CRUSHER TNH LIAT T3",
+                "KILN T3 - SIE RKC3",
+                "RAW MILL T3 SIE RKC3"
+)
+
+#Daftar lokasi ovh kiln 4
+area_kiln4 <- c("COAL MILL T4SIE RKC4",
+                "CRUSHER BT. KAPUR T4",
+                "CRUSHER TNH LIAT T4",
+                "KILN T4 - SIE RKC4",
+                "RAW MILL T4 SIE RKC4"
+)
+
+
+
+#menambahkan variabel OVH_AREA
+order_area <- order_area %>%
+  mutate(kategori_ovh = case_when(
+    Jenis.aktivitas == "Overhoule" & Detail.area %in% area_kiln1 ~ "Overhoule Kiln 1",
+    Jenis.aktivitas == "Overhoule" & Detail.area %in% area_kiln2 ~ "Overhoule Kiln 2",
+    Jenis.aktivitas == "Overhoule" & Detail.area %in% area_kiln3 ~ "Overhoule Kiln 3",
+    Jenis.aktivitas == "Overhoule" & Detail.area %in% area_kiln4 ~ "Overhoule Kiln 4",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 1 TBN 1" ~ "Overhoule Mill 1",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 1 TBN 2" ~ "Overhoule Mill 3",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 1 TBN 3" ~ "Overhoule Mill 5",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 1 TBN 4" ~ "Overhoule Mill 7",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 2 TBN 1" ~ "Overhoule Mill 2",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 2 TBN 2" ~ "Overhoule Mill 4",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 2 TBN 3" ~ "Overhoule Mill 6",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 2 TBN 4" ~ "Overhoule Mill 8",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "FINISH MILL 9 TBN" ~ "Overhoule Mill 9",
+    Jenis.aktivitas == "Overhoule" & Detail.area == "NEW COAL MILL T123" ~ "Overhoule Kiln 1",
+    Jenis.aktivitas == "Overhoule" & PLANT == "Penunjang Tuban" ~ "Overhoule Penunjang Tuban",
+    Jenis.aktivitas == "Overhoule" & Area.Proses == "Packer" ~ "Operasional",
+    Jenis.aktivitas == "Patchjob" ~ "Patchjob",
+    TRUE ~ "Operasional"))
 
 
 #Memilih kolom kob yang dipakai
